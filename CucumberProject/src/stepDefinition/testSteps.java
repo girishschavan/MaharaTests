229783cfffe93a0db0ecdcf43ff16ed176623d98
Girish Chavan <girishschavan@gmail.com>
2016-06-18 21:09:54 -0700

    1

--- CucumberProject/src/stepDefinition/testSteps.java
+++ CucumberProject/src/stepDefinition/testSteps.java
@@ -21,7 +21,7 @@ public class testSteps {
 		//driver = new FirefoxDriver();
 		driver.get("http://demo.mahara.org/");
 		driver.manage().window().maximize();
-		System.out.println("browser opened");
+		System.out.println("browser opened1");
 		// Write code here that turns the phrase above into concrete actions
 	    // throw new PendingException();
 	}
